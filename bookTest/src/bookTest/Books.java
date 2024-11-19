e2b9b8da44f9eb2c78c1f30a667f71d899048b39
Cwoobin <wohyuck@naver.com>
2024-11-19 16:17:10 +0900

    Books 프로그램 print, insert, update, delete, exit 테스트

---
+++ bookTest/src/bookTest/Books.java
@@ -0,0 +1,54 @@
+package bookTest;
+
+public class Books {
+	private int id;
+	private String title;
+	private String publisher;
+	private String year;
+	private int price;
+	public Books(int id, String title, String publisher, String year, int price) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.publisher = publisher;
+		this.year = year;
+		this.price = price;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getPublisher() {
+		return publisher;
+	}
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+	public String getYear() {
+		return year;
+	}
+	public void setYear(String year) {
+		this.year = year;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	@Override
+	public String toString() {
+		return "Books [id=" + id + ", title=" + title + ", publisher=" + publisher + ", year=" + year + ", price="
+				+ price + "]";
+	}
+	
+	
+}
